ce6f848188a95386904fc51f96e74be630900992
Luis Bernal <luis_bernal8010@yahoo.com>
2020-04-12 16:43:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(sentence1)
   sentence1 = sentence1.split
   reversed = []
   sentence1.each do |x|
-    x.reverse << reversed
+    x.reverse
+    sentence1 = reversed
 end
 reversed
 end
